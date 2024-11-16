0d5c4e8a7c7c0871d90d9fca619088e072c55962
ReapingHunter <manalilimarlex@gmail.com>
2024-11-16 19:35:28 +0800

    fix: tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,5 @@
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +7,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+      </Routes>
+    </Router>
   </StrictMode>,
 )
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,44 @@
 
-export default function LoginPage(){
-  return(
+export default function LoginPage() {
+  return (
     <>
-      <div>
-        
+      <div className="flex h-screen">
+        {/* Left half with background image */}
+        {/* <div className="w-1/2 h-full bg-cover bg-center" style={{ backgroundImage: "url('/images/login-bg.svg')" }} /> */}
+
+        {/* Right half for content */}
+        <div className="w-1/2 bg-white flex flex-col justify-center items-center">
+          <h1 className="text-6xl font-bold mb-4">Login</h1>
+          <p className="text-sm text-[#737272] font-normal whitespace-nowrap">Don&apos;t have an account yet?
+            <a className="text-[#7B00D4] font-normal"> Signup</a>.
+          </p>
+          <form action="#" method="post">
+            <div className="mb-4 mt-4">
+              <label htmlFor="email" className="block text-sm font-bold text-[#363636] mb-2">
+                Email Address
+              </label>
+              <input 
+                type="email"
+                id="email"
+                name="email"
+                className="bg-transparent outline-none border-2 border-[#363636] rounded-md mb-3 px-3 py-2"
+                required>
+              </input>
+              <label htmlFor="password" className="block text-sm font-bold text-[#363636] mb-2">
+                Password
+              </label>
+              <input 
+                type="password"
+                id="password"
+                name="password"
+                className="bg-transparent outline-none border-2 border-[#363636] rounded-md mb-5 px-3 py-2"
+                required>
+              </input>
+              <button className="font-bold bg-[#7B00D4] text-white px-4 py-2 rounded-md">LOGIN</button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
-  )
+  );
 }
