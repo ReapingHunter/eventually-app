954fe523edd5d2724add616f8badf49dceb57644
L0rd.Inquisit0r <143763707+L0rd-Inquisit0r@users.noreply.github.com>
2024-12-01 20:39:52 +0800

    fix: updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ error: 'Something went wrong!' });
 });
 
 app.listen(PORT, () => {
