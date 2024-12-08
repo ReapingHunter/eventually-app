0604e72bba57e80ff300c321859b8f22cb4d5329
ReapingHunter <manalilimarlex@gmail.com>
2024-12-08 23:19:23 +0800

    adjusted font size for h1 in AllEventsPage

--- frontend/src/pages/AllEventsPage.jsx
+++ frontend/src/pages/AllEventsPage.jsx
@@ -30,7 +30,7 @@ export default function AllEventsPage() {
     <>
       <NavBar />
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-8">All Events</h1>
+        <h1 className="text-5xl font-bold mb-8">All Events</h1>
         <FilterEvents />
 
         {/* Event Grid */}
