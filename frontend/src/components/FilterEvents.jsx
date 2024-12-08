020fe0a23498fa8f81f53ddd2aab8aea699ac3d7
ReapingHunter <manalilimarlex@gmail.com>
2024-12-08 23:16:00 +0800

    add: date range picker for filter events

--- frontend/src/components/FilterEvents.jsx
+++ frontend/src/components/FilterEvents.jsx
@@ -1,8 +1,7 @@
-
-import { Input } from "./ui/input"
-import { Button } from "./ui/button"
-import { Check, ChevronDown } from "lucide-react"
-import { cn } from "@/lib/utils"
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Check, ChevronDown, Calendar as CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 import {
   Command,
   CommandEmpty,
@@ -10,56 +9,72 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from "@/components/ui/command"
+} from "@/components/ui/command";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-import { useState } from "react"
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+import { addDays, format } from "date-fns";
 
 const locations = [
   {
     value: "cebu",
-    label: "Cebu"
+    label: "Cebu",
   },
   {
     value: "bohol",
-    label: "Bohol"
+    label: "Bohol",
   },
-]
+];
 
 const categories = [
   {
     value: "networking",
-    label: "Networking"
+    label: "Networking",
   },
   {
     value: "wedding",
-    label: "Wedding"
+    label: "Wedding",
   },
-]
-export default function FilterEvents(){
-  const [isLocationOpen, setLocationOpen] = useState(false)
-  const [locationValue, setLocationValue] = useState("")
+];
 
-  const [isCategoryOpen, setCategoryOpen] = useState(false)
-  const [categoryValue, setCategoryValue] = useState("")
-  return(
+export default function FilterEvents() {
+  const [isLocationOpen, setLocationOpen] = useState(false);
+  const [locationValue, setLocationValue] = useState("");
+
+  const [isCategoryOpen, setCategoryOpen] = useState(false);
+  const [categoryValue, setCategoryValue] = useState("");
+
+  const [dateRange, setDateRange] = useState({
+    from: new Date(),
+    to: addDays(new Date(), 7),
+  });
+
+  return (
     <>
       <div className="flex bg-[#fdfdfd] border-2 px-4 py-4 items-center justify-center drop-shadow-xl w-full mb-4 gap-4">
-        <Input className="bg-[#f7f7f7] border-0 rounded-none shadow-inner w-1/2" placeholder="Search Event name" />
+        {/* Event Name Filter */}
+        <Input
+          className="bg-[#f7f7f7] border-0 rounded-none shadow-inner w-1/2"
+          placeholder="Search Event name"
+        />
 
-        {/* Event Location */}
+        {/* Event Location Filter */}
         <Popover open={isLocationOpen} onOpenChange={setLocationOpen}>
           <PopoverTrigger asChild>
-            <Button 
+            <Button
               variant="outline"
               role="combobox"
               aria-expanded={isLocationOpen}
-              className="bg-[#f7f7f7] mb-4 rounded-none border">
-                {locationValue ? locations.find((location) => location.value === locationValue)?.label : "Select Location..."}
-                <ChevronDown className="text-[#7b00d4]" />
+              className="bg-[#f7f7f7] mb-4 rounded-none border"
+            >
+              {locationValue
+                ? locations.find((location) => location.value === locationValue)?.label
+                : "Select Location..."}
+              <ChevronDown className="text-[#7b00d4]" />
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-[200px] p-0">
@@ -73,8 +88,8 @@ export default function FilterEvents(){
                       key={location.value}
                       value={location.value}
                       onSelect={(currentVal) => {
-                        setLocationValue(currentVal === locationValue ? "" : currentVal)
-                        setLocationOpen(false)
+                        setLocationValue(currentVal === locationValue ? "" : currentVal);
+                        setLocationOpen(false);
                       }}
                     >
                       {location.label}
@@ -82,7 +97,7 @@ export default function FilterEvents(){
                         className={cn(
                           "ml-auto",
                           locationValue === location.value ? "opacity-100" : "opacity-0"
-                        )} 
+                        )}
                       />
                     </CommandItem>
                   ))}
@@ -92,16 +107,19 @@ export default function FilterEvents(){
           </PopoverContent>
         </Popover>
 
-        {/* Event Categories */}
+        {/* Event Category Filter */}
         <Popover open={isCategoryOpen} onOpenChange={setCategoryOpen}>
           <PopoverTrigger asChild>
-            <Button 
+            <Button
               variant="outline"
               role="combobox"
               aria-expanded={isCategoryOpen}
-              className="bg-[#f7f7f7] mb-4 rounded-none border">
-                {categoryValue ? categories.find((category) => category.value === categoryValue)?.label : "Select Category..."}
-                <ChevronDown className="text-[#7b00d4]" />
+              className="bg-[#f7f7f7] mb-4 rounded-none border"
+            >
+              {categoryValue
+                ? categories.find((category) => category.value === categoryValue)?.label
+                : "Select Category..."}
+              <ChevronDown className="text-[#7b00d4]" />
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-[200px] p-0">
@@ -115,8 +133,8 @@ export default function FilterEvents(){
                       key={category.value}
                       value={category.value}
                       onSelect={(currentVal) => {
-                        setCategoryValue(currentVal === categoryValue ? "" : currentVal)
-                        setCategoryOpen(false)
+                        setCategoryValue(currentVal === categoryValue ? "" : currentVal);
+                        setCategoryOpen(false);
                       }}
                     >
                       {category.label}
@@ -124,7 +142,7 @@ export default function FilterEvents(){
                         className={cn(
                           "ml-auto",
                           categoryValue === category.value ? "opacity-100" : "opacity-0"
-                        )} 
+                        )}
                       />
                     </CommandItem>
                   ))}
@@ -133,7 +151,44 @@ export default function FilterEvents(){
             </Command>
           </PopoverContent>
         </Popover>
+
+        {/* Date Range Picker */}
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              className={cn(
+                "bg-[#f7f7f7] mb-4 rounded-none border w-[300px] justify-start text-left font-normal",
+                !dateRange && "text-muted-foreground"
+              )}
+            >
+              <CalendarIcon />
+              {dateRange?.from ? (
+                dateRange.to ? (
+                  <>
+                    {format(dateRange.from, "LLL dd, y")} -{" "}
+                    {format(dateRange.to, "LLL dd, y")}
+                  </>
+                ) : (
+                  format(dateRange.from, "LLL dd, y")
+                )
+              ) : (
+                <span>Select a date range...</span>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              initialFocus
+              mode="range"
+              defaultMonth={dateRange?.from}
+              selected={dateRange}
+              onSelect={setDateRange}
+              numberOfMonths={2}
+            />
+          </PopoverContent>
+        </Popover>
       </div>
     </>
-  )
+  );
 }
