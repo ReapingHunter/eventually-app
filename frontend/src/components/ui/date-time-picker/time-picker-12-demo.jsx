7127dabc526c37205c12a6bf4da202738fc2ceff
ReapingHunter <manalilimarlex@gmail.com>
2024-12-12 23:25:41 +0800

    add: time picker 12 hour demo

---
+++ frontend/src/components/ui/date-time-picker/time-picker-12-demo.jsx
@@ -0,0 +1,82 @@
+"use client";
+
+import React, { useRef, useState } from "react";
+import PropTypes from "prop-types";
+import { Label } from "@/components/ui/label";
+import { TimePickerInput } from "./time-picker-input";
+import { TimePeriodSelect } from "./period-select";
+
+TimePicker12Demo.propTypes = {
+  date: PropTypes.instanceOf(Date), // Validates that `date` is an instance of Date or undefined
+  setDate: PropTypes.func.isRequired, // Validates that `setDate` is a required function
+};
+
+function TimePicker12Demo({ date, setDate }) {
+  const [period, setPeriod] = useState("PM");
+
+  const minuteRef = useRef(null);
+  const hourRef = useRef(null);
+  const secondRef = useRef(null);
+  const periodRef = useRef(null);
+
+  return (
+    <div className="flex items-end gap-2">
+      <div className="grid gap-1 text-center">
+        <Label htmlFor="hours" className="text-xs">
+          Hours
+        </Label>
+        <TimePickerInput
+          picker="12hours"
+          period={period}
+          date={date}
+          setDate={setDate}
+          ref={hourRef}
+          onRightFocus={() => minuteRef.current?.focus()}
+        />
+      </div>
+      <div className="grid gap-1 text-center">
+        <Label htmlFor="minutes" className="text-xs">
+          Minutes
+        </Label>
+        <TimePickerInput
+          picker="minutes"
+          id="minutes12"
+          date={date}
+          setDate={setDate}
+          ref={minuteRef}
+          onLeftFocus={() => hourRef.current?.focus()}
+          onRightFocus={() => secondRef.current?.focus()}
+        />
+      </div>
+      <div className="grid gap-1 text-center">
+        <Label htmlFor="seconds" className="text-xs">
+          Seconds
+        </Label>
+        <TimePickerInput
+          picker="seconds"
+          id="seconds12"
+          date={date}
+          setDate={setDate}
+          ref={secondRef}
+          onLeftFocus={() => minuteRef.current?.focus()}
+          onRightFocus={() => periodRef.current?.focus()}
+        />
+      </div>
+      <div className="grid gap-1 text-center">
+        <Label htmlFor="period" className="text-xs">
+          Period
+        </Label>
+        <TimePeriodSelect
+          period={period}
+          setPeriod={setPeriod}
+          date={date}
+          setDate={setDate}
+          ref={periodRef}
+          onLeftFocus={() => secondRef.current?.focus()}
+        />
+      </div>
+    </div>
+  );
+}
+
+export { TimePicker12Demo };
