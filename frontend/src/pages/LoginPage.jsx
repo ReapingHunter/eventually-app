36b0b30c2c7a4f79bdf0eedb79eda7295254ad82
ReapingHunter <manalilimarlex@gmail.com>
2024-12-13 10:22:51 +0800

    adjusted padding for login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,12 +1,24 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-
+import { useState, useEffect } from "react";
 export default function LoginPage() {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // Adjust the breakpoint (e.g., 768px for mobile)
+    };
+
+    handleResize(); // Initial check
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [])
   return (
     <>
       <div style={{ backgroundImage: "url('/images/login-bg.svg')" }} className="min-h-screen bg-cover bg-center">
-        <div className="flex h-screen px-4 md:px-16 lg:px-32 py-16">
+        <div className="flex h-screen px-56 py-24">
           {/* Flex container with rounded corners */}
           <div className="flex h-full w-full rounded-2xl overflow-hidden shadow-lg">
             {/* Left half with background image */}
@@ -16,7 +28,13 @@ export default function LoginPage() {
             />
             
             {/* Right half for content */}
-            <div className="w-full md:w-1/2 h-full bg-white flex flex-col justify-center lg:items-start md:items-center sm:items-center px-8 sm:px-16 md:px-20 lg:px-28">
+            <div className="w-full md:w-1/2 h-full bg-white flex flex-col justify-center items-start px-20">
+              {isMobile && <img
+                              src="/images/dashboard-logo-2.png"
+                              className="w-72 h-auto"
+                              alt="Dashboard Logo"
+                            />
+              }
               <h1 className="text-3xl md:text-5xl font-bold mb-2">Login</h1>
               <p className="text-sm md:text-base text-[#737272] font-normal whitespace-nowrap mb-4">
                 Don&apos;t have an account yet?
@@ -30,8 +48,8 @@ export default function LoginPage() {
                   <Input type="email" id="email" placeholder="Enter your email" />
                   <Label htmlFor="password">Password</Label>
                   <Input type="password" id="password" placeholder="Enter your password" />
-                  <div className="flex flex-col mt-4 lg:items-end md:items-center sm:items-center">
-                    <Button className="font-bold w-full lg:w-1/2 md:w-1/2 bg-[#7B00D4] text-white hover:bg-[#5b00a6]">
+                  <div className="flex flex-col mt-4 items-end">
+                    <Button className="font-bold w-1/2 bg-[#7B00D4] text-white hover:bg-[#5b00a6]">
                       LOGIN
                     </Button>
                     <a
