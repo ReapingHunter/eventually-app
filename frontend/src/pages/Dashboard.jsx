35df25f22d6f9667ebee392f0f305e397efbef71
ReapingHunter <manalilimarlex@gmail.com>
2024-12-14 10:38:09 +0800

    changed register button color when hovered

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -39,7 +39,7 @@ export default function Dashboard() {
             </div>
             {/* Register Button */}
             <a href="/signup">
-              <Button className="flex bg-gradient-to-r from-[#7b00d4] via-[#A255DA] to-[#F03CF9] hover:from-[#5a00a8] hover:to-[#c42cf0] transition text-white font-semibold py-2 px-4 sm:px-6 rounded-lg shadow-md gap-2">
+              <Button className="flex bg-gradient-to-r from-[#7b00d4] via-[#A255DA] to-[#F03CF9] hover:brightness-110 transition text-white font-semibold py-2 px-4 sm:px-6 rounded-lg shadow-md gap-2">
                 <SparklesIcon className="w-5 h-5" />
                 Register now
               </Button>
