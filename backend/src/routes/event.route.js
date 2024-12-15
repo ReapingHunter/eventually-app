210b4a0992173a07e1fdc952b9e7e9a179f05740
ReapingHunter <manalilimarlex@gmail.com>
2024-12-15 20:38:13 +0800

    add: get event by user route

--- backend/src/routes/event.route.js
+++ backend/src/routes/event.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createEvent, getAllEvents, getEventById, getEventByFilter, updateEvent } from '../controllers/event.controller.js';
+import { createEvent, getAllEvents, getEventById, getEventByFilter, getEventByUser, updateEvent } from '../controllers/event.controller.js';
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post('/create-event', createEvent); // Create an event
 router.get('/all-event', getAllEvents); // Get all events
 router.get('/:id', getEventById); // Get a specific event by ID
 router.get('/filter-event', getEventByFilter);
+router.get('/user-event', getEventByUser);
 router.get('/update-event', updateEvent);
 
 export default router;
